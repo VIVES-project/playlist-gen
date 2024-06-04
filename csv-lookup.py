1de978611f087008e298c9f4bf0d58c0d483625a
Mediavilla, Lino <linomoises.mediavillaponce@sew-eurodrive.it>
2024-06-04 17:09:27 +0200

    add pseudocode

--- csv-lookup.py
+++ csv-lookup.py
@@ -4,11 +4,17 @@ from models import ImageCaption, SongLookupResult
 def get_top_songs(caption: ImageCaption)->list[SongLookupResult]:
     print(f"Searching songs of the genres: {caption.top_music_genres}, with tags: {caption.top_tags}")
 
-    return []
+    results = []
+
+    # open csv and read line by line
+    # if genre is not in caption.top_music_genres or topic is not in caption.top_tags, skip
+    # else, add to results
+
+    return results
 
 
 if __name__ == "__main__":
-    test_input = '{"description": "The person is wearing a casual white t-shirt with light blue horizontal stripes. The outfit, coupled with his relaxed posture and the indoor setting, suggests a laid-back, approachable vibe.", "top_music_genres": ["indie", "alternative rock"],"top_tags": ["world/life", "night/time", "feelings"]}'
+    test_input = '{"description": "The person is wearing a casual white t-shirt with light blue horizontal stripes. The outfit, coupled with his relaxed posture and the indoor setting, suggests a laid-back, approachable vibe.", "top_music_genres": ["rock", "jazz"],"top_tags": ["world/life", "night/time", "feelings"]}'
     test_input = ImageCaption(**eval(test_input))
 
     print(get_top_songs(test_input))
