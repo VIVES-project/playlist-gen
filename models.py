9442cf16835523d7923e6231c5178443390b9959
nicoaspra <63556338+nicoaspra@users.noreply.github.com>
2024-06-05 23:52:25 +0200

    imgCaption energy level and tempo

--- models.py
+++ models.py
@@ -3,8 +3,9 @@ from pydantic import BaseModel
 class ImageCaption(BaseModel):
     description: str
     top_music_genres: list[str]
-    top_tags: list[str]
     title_playlist: str
+    music_energy: str
+    music_tempo: str
 
 class SongLookupResult(BaseModel):
     artist: str
