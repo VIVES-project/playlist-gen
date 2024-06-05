627b975248d777e936859b9460b70a9684b4a170
nicoaspra <63556338+nicoaspra@users.noreply.github.com>
2024-06-05 16:27:28 +0200

    update csv lookup to output 5 random

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ class ImageCaption(BaseModel):
     description: str
     top_music_genres: list[str]
     top_tags: list[str]
+    title_playlist: str
 
 class SongLookupResult(BaseModel):
     artist: str
