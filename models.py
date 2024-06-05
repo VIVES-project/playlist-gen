a6e7040a0b87a3a40c1f1322957c236281d592a9
Sherry-jsp <pjhoannasherry28@gmail.com>
2024-06-05 23:52:51 +0200

    refactor(weather): gets recent data

--- models.py
+++ models.py
@@ -1,11 +1,20 @@
 from pydantic import BaseModel
 
+
 class ImageCaption(BaseModel):
     description: str
     top_music_genres: list[str]
     top_tags: list[str]
     title_playlist: str
 
+
 class SongLookupResult(BaseModel):
     artist: str
     song_name: str
+
+
+class WeatherData(BaseModel):
+    date: str
+    time: str
+    kind: str
+    temp: str
